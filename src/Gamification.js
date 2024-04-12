d5aea0afd6bbf953caf8dd60632bc871e9e6114e
Bharat Kathore <bharat@nilede.tech>
2024-04-12 13:17:05 +0530

    orientation added

--- src/Gamification.js
+++ src/Gamification.js
@@ -5,22 +5,26 @@ import base64 from 'base-64';
 
 const renderHtml = async (finalUrl) => {
     const dynamicContent = `
-                <div onclick="customClickHandler('gamification_${config.id}')" id="frame_close_${config.id}" style="height: 36px; width: 33px;
-                 position: absolute; background: 'red'; background-color: 'red'; right: 7px; top: 25px;"></div>
+                <div onclick="customClickHandler('gamification_${config.id}')" 
+                id="frame_close_${config.id}" 
+                style="height: 36px; width: 33px;
+                 position: absolute; right: 7px; top: 25px;z-index: 99999"></div>
                   <iframe
                 id="frame_gamification_${config.id}" src="${finalUrl}"
-                style="height: 100vh;width:100%;border:none;"/>
+                style="height:${config?.style?.height ?? '100vh'};width:100%;border:none;background: #f1f1f1"/>
         `;
     // Create a temporary container element
     const tempContainer = document.createElement('div');
     tempContainer.innerHTML = dynamicContent;
-    tempContainer.style.height = '100vh';
-    tempContainer.style.width = '100%';
-    tempContainer.style.zIndex = '99999';
-    tempContainer.style.top = '0px';
-    tempContainer.style.left = '0px';
+    tempContainer.style.height = config?.style?.height ?? '100vh';
+    tempContainer.style.width = config?.style?.width ?? '100%';
+    tempContainer.style.zIndex = config?.style?.zIndex ?? '99999';
+    tempContainer.style.top = config?.style?.top ?? '0px';
+    tempContainer.style.left = config?.style?.left ?? '0px';
     tempContainer.style.position = 'absolute';
-    tempContainer.id = `gamification_${config.id}`
+    tempContainer.style.backgroundColor = '#f1f1f1';
+    tempContainer.id = `gamification_${config.id}`;
+
     // Append the content inside the first div
     const renderedContent = document.getElementById(config.appId);
     renderedContent.appendChild(tempContainer);
@@ -28,13 +32,32 @@ const renderHtml = async (finalUrl) => {
     document.body.appendChild(renderedContent);
 
     const scriptElement = document.createElement('script');
+
+
     scriptElement.innerHTML = `
     function customClickHandler(id) {
         const dynamicDiv = document.getElementById(id);
-    if (dynamicDiv) {
-        dynamicDiv.parentNode.removeChild(dynamicDiv);
+        if (dynamicDiv) {
+            dynamicDiv.parentNode.removeChild(dynamicDiv);
+        }
     }
-    }
+    
+    
+    document.addEventListener("DOMContentLoaded", function() {
+    alert('ok')
+    var myFrame = document.getElementById("frame_gamification_${config.id}");
+    console.log(myFrame);
+    myFrame.addEventListener("load", function() {
+        var iframeDoc = myFrame.contentDocument || myFrame.contentWindow.document;
+        var divElements = iframeDoc.getElementsByTagName("div");
+        for (var i = 0; i < divElements.length; i++) {
+            divElements[i].addEventListener("click", function() {
+                alert("clicked");
+            });
+        }
+    });
+});
+
 `;
     document.body.appendChild(scriptElement);
 }
@@ -57,7 +80,8 @@ const Gamification = {
         utm_param2: "",
         utm_param3: "",
         utm_param4: "",
-        appId: 'root'
+        appId: 'root',
+        style: {height: '100vh'}
     }) => {
 
         config = data;
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,7 +3,7 @@
     <ion-content :fullscreen="true">
       <div style="padding: 10px">
         <IonButton @click="openApp" type="button" mode="md" color="primary">
-          Test
+          IONIC Test
         </IonButton>
       </div>
     </ion-content>
@@ -13,6 +13,8 @@
 <script setup>
 import {IonButton, IonContent, IonPage} from '@ionic/vue';
 import Gamification from "ionic-gamification-test";
+import {ref} from "vue";
+import {ScreenOrientation} from '@capacitor/screen-orientation';
 
 Gamification.init({
   baseUrl: "https://thelogicalbanya.com/popupdemo/dashboard.php",
@@ -21,10 +23,35 @@ Gamification.init({
   userID: "100031",
   username: "TheLogicalBanya",
   keyString: "bR5z6*r$00p#Eno__odrEgeW",
-  appId: 'app' /* pass div id in used index.html like  <body> <div id="app"></div>< /body> */
+  appId: 'app', /* pass div id in used index.html like  <body> <div id="app"></div>< /body> */
+  // style: {
+  //   height: 'calc(100vh - 70px)',
+  //   top: '70px',
+  // }
 });
+const isOpen = ref(false);
 
-const openApp = () => {
-  Gamification.run();
+const openApp = async () => {
+  try {
+    await ScreenOrientation.lock({orientation: 'portrait'});
+  } catch (e) {
+    console.error(e.message)
+  }
+
+  await Gamification.run();
+  isOpen.value = true;
+
+  // for close gamification window
+  const divElement = document.querySelector('.close_btn');
+  divElement.addEventListener('click', async () => {
+    await Gamification.close(divElement.dataset.id);
+    isOpen.value = false;
+    try {
+      await ScreenOrientation.unlock();
+    } catch (e) {
+      console.error(e.message)
+    }
+  });
+
 }
 </script>
